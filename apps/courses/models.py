71ae403c5b0f03e39c1dd01aff2079fa225f1df3
dongwenda <1055426453@qq.com>
2018-10-18 01:03:09 +0800

    models表创建

---
+++ apps/courses/models.py
@@ -0,0 +1,54 @@
+# -*- encoding:utf-8 -*-
+from datetime import datetime
+
+from django.db import models
+
+# Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50,verbose_name='课程名称')
+    desc = models.CharField(max_length=300,verbose_name='课程描述')
+    detail = models.TextField(verbose_name='课程详情')
+    degree = models.CharField(choices=(('cj','初级'),('zj','中级'),('gj','高级')),max_length=2)
+    learn_times = models.IntegerField(default=0,verbose_name='学习时长（分钟数）')
+    students = models.IntegerField(default=0,verbose_name='学习人数')
+    fav_nums = models.IntegerField(default=0,verbose_name='收藏人数')
+    image = models.ImageField(upload_to='courses/%Y/%m',verbose_name='封面图',max_length=100)
+    click_nums = models.IntegerField(default=0,verbose_name='点击数')
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程'
+        verbose_name_plural = verbose_name
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, verbose_name='课程')  # 一对多，多对一都是用外键来关联
+    name = models.CharField(max_length=100, verbose_name='章节名')
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '章节'
+        verbose_name_plural = verbose_name
+
+
+class Video(models.Model):
+    lesson = models.ForeignKey(Lesson, verbose_name='章节')
+    name = models.CharField(max_length=100, verbose_name='视频名称')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '视频'
+        verbose_name_plural = verbose_name
+
+
+class CourseResource(models.Model):
+    course = models.ForeignKey(Course, verbose_name='课程')
+    name = models.CharField(max_length=100, verbose_name='资源名称')
+    download = models.FileField(upload_to='course/resource/%Y/%m',verbose_name='资源文件',max_length=100) # 使用FileField 在后台xadmin管理系统中就展示的上传按钮
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程资源'
+        verbose_name_plural = verbose_name
